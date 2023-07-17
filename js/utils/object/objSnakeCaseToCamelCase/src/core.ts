8c106619fa83784c471e23052d3d80d1ce6ba78d
tokkiumai <kiwidancebad@gmail.com>
2023-07-17 20:48:00 +0300

    types test

--- js/utils/object/objSnakeCaseToCamelCase/src/core.ts
+++ js/utils/object/objSnakeCaseToCamelCase/src/core.ts
@@ -1,46 +1,72 @@
 import { strSnakeToCamelCase } from '@tkku/strsnakecasetocamelcase'
 
-export function objSnakeToCamelCase(
-  obj: any,
-  exclude: string | string[] = [],
-  nestedKey = '',
-): any {
-  let result: Record<string, any> = {}
+type SnakeToCamelCase<S> =
+  S extends `${infer T}_${infer U}_${infer V}` ?
+    `${T}${Capitalize<U>}${Capitalize<SnakeToCamelCase<V>>}` :
+    S extends `${infer T}_${infer U}` ?
+      `${T}${Capitalize<SnakeToCamelCase<U>>}` :
+      S
+
+type ObjSnakeToCamelCaseSingle<O> = O extends object ? {
+  [K in keyof O as SnakeToCamelCase<K>]: O[K]
+} : O
+
+// type ObjSnakeToCamelCaseNested<T> = T extends object ? {
+//   [K in keyof T as SnakeToCamelCase<K & string>]: ObjSnakeToCamelCaseNested<T[K]>
+// } : T
+
+type ObjSnakeToCamelCaseNested<O> = O extends object ? {
+  [K in keyof O as SnakeToCamelCase<K>]: ObjSnakeToCamelCaseNested<O[K]>
+} : O
+
+/** test */
+const obj = {
+  // [{ key_1: 'a' }],
+  key_2: 'afsafg',
+  key_3: {
+    key_4: 'bsfafa'
+  }
+}
+type B = ObjSnakeToCamelCaseNested<typeof obj>
+
+/**
+ * Recursively replaces the snake cased keys in an object with camel cased
+ * To exclude a keys - provide second "exclude" argument (string | Array<string)
+ *
+ * Returns null if passed objects is not an instance of Object
+ *
+ * @returns Object | null
+ * */
+export function objSnakeToCamelCase(obj: any, exclude: string | string[] = [], nestedKey = ''): any {
+  if (!(obj instanceof Object)) {
+    return null
+  }
+  let result: any = {}
   let excluded = Array.isArray(exclude) ? exclude : [exclude]
-  for (const [key, value] of Object.entries(obj)) {
+  for (let [key, value] of Object.entries(obj)) {
     if (excluded.includes(key)) {
       result[key] = value
       continue
     }
-    if (
-      nestedKey &&
-      excluded.some(excludedKey => excludedKey === `${nestedKey}.${key}`)
-    ) {
+    if (nestedKey && excluded.some(excludedKey => excludedKey === `${nestedKey}.${key}`)) {
       result[key] = value
       continue
     }
+    let _key = strSnakeToCamelCase(key)
     if (value instanceof Object) {
       if (Array.isArray(value)) {
-        result[strSnakeToCamelCase(key)] = value.map(item => {
+        result[_key] = value.map(item => {
           if (item instanceof Object) {
-            return objSnakeToCamelCase(
-              item,
-              excluded,
-              nestedKey ? `${nestedKey}.${key}` : key,
-            )
+            return objSnakeToCamelCase(item, excluded, nestedKey ? `${nestedKey}.${key}` : key)
           }
           return item
         })
         continue
       }
-      result[strSnakeToCamelCase(key)] = objSnakeToCamelCase(
-        value,
-        excluded,
-        nestedKey ? `${nestedKey}.${key}` : key,
-      )
+      result[_key] = objSnakeToCamelCase(value, excluded, nestedKey ? `${nestedKey}.${key}` : key)
       continue
     }
-    result[strSnakeToCamelCase(key)] = value
+    result[_key] = value
   }
   return result
 }
